f4febf3a3a6f270ff514ba66fca067c8c7b695c2
Usuario <Usuario@DESKTOP-DGNAOIQ>
2020-12-28 21:14:06 +0000

    Kata3 version 1

---
+++ src/kata3/Main.java
@@ -0,0 +1,13 @@
+package kata3;
+
+/**
+ * @author Antonio Miguel Martel
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        HistogramDisplay histo = new HistogramDisplay();
+        histo.execute();
+    }
+
+}
